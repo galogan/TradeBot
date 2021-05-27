a2c2fb0a5be7ebdfdcece2e7fbf9fe6ac261f25b
galogan <galogan02@gmail.com>
2021-05-26 20:57:41 -0500

    Indicator WIP: calculating price change

--- pyrobot/indicator.py
+++ pyrobot/indicator.py
@@ -2,11 +2,14 @@ import numpy as np
 import pandas as pd
 import operator
 
+from typing import Any
 from typing import List
 from typing import Dict
 from typing import Union
 from typing import Optional
 from typing import Tuple
+
+from pandas.core.frame import DataFrame
 
 from pyrobot.stock_frame import StockFrame
 
@@ -15,4 +18,79 @@ class Indicator():
     def _init_(self, price_data_frame: StockFrame) -> None:
 
         self._stock_frame: StockFrame = price_data_frame
-        self._price_groups = price_data_frame.symbol_groups
+        self._price_groups = self._stock_frame.symbol_groups
+        self._current_indicators = {}
+        self._indicator_signals = {}
+        self._frame = self._stock_frame.frame
+
+    def set_indicator_signals(self, indicator: str, buy: float, sell: float, condition_buy: Any, condition_sell: Any) -> None:
+
+        # If there is no signal for that indicator set a template.
+        if indicator in self._indicator_signals:
+            self._indicator_signals[indicator] = {}
+
+        # Modify the signal
+        self._indicator_signals[indicator]['buy'] = buy
+        self._indicator_signals[indicator]['sell'] = sell
+        self._indicator_signals[indicator]['buy_operator'] = condition_buy
+        self._indicator_signals[indicator]['sell_operator'] = condition_sell
+
+    def get_indicator_signals(self, indicator: Optional[str]) -> Dict:
+
+        if indicator and indicator in self._indicator_signals:
+            return self._indicator_signals[indicator]
+        else:
+            return self._indicator_signals
+
+    @property
+    def price_data_frame(self) ->pd.DataFrame: 
+
+        return self._frame
+
+    @price_data_frame.setter
+    def price_data_frame(self, price_data_frame: pd.DataFrame) -> None:
+
+        self._frame = price_data_frame
+
+    def change_in_price(self) -> pd.DataFrame:
+
+        locals_data = locals()
+        del locals_data['self']
+
+        column_name = 'change_in_Pice'
+        self._current_indicators[column_name] = {}
+        self._current_indicators[column_name]['args'] = locals_data
+        self._current_indicators[column_name]['func'] = self.change_in_price
+
+        self._frame[column_name] = self._price_groups['close'].transform(
+            lambda x: x.diff()
+        )
+
+    def rsi(self, period: int, method: str = 'wilders') -> pd.DataFrame:
+
+        locals_data = locals()
+        del locals_data['self']
+
+        column_name = 'rsi'
+        self._current_indicators[column_name] = {}
+        self._current_indicators[column_name]['args'] = locals_data
+        self._current_indicators[column_name]['func'] = self.rsi
+
+        if 'change_in_price' not in self._frame.column:
+            self.change_in_price()
+
+        # Define the up days.
+        self._frame['up_day'] = self._price_groups['change_in_price'].transform(
+            lambda x : np.where(x >=0, x, 0)
+        )
+        self._frame['down_day'] = self._price_groups['change_in_price'].transform(
+            lambda x: np.where(x < 0, x.abs(), 0)
+        )
+        self._frame['ewma_up'] = self._price_groups['up_day'].transform(
+            lambda x: x.ewm(span=period).mean()
+        )
+        self._frame['ewma_down'] = self._price_groups['down_day'].transform(
+            lambda x: x.ewm(span=period).mean()
+        )
+
+        relative_strength = self._frame['ewma_up'] / self._frame['ewma_down']
